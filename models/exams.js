6f48676326af16f0949385469392036654892a01
joeljosephreji <joeljosephreji@gmail.com>
2018-10-25 23:26:58 +0530

    added marks to the exam table

--- models/exams.js
+++ models/exams.js
@@ -21,6 +21,11 @@ module.exports = function (sequelize, DataTypes) {
           allowNull: false,
           unique: 'compositeIndex',
         },
+        marks: {
+          type: DataTypes.INTEGER(),
+          allowNull: true,
+          unique: 'compositeIndex',
+        },
       // implement [index6 specified in db design] compositeIndex when sequelize
       // supports it.
       // https://github.com/sequelize/sequelize/issues/8148
