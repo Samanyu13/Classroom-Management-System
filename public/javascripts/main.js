358405a6f0539c41fec708499b2ac535b7fb0452
Samanyu13 <samanyusaji18@gmail.com>
2018-10-27 14:28:32 +0530

    Optimised dropdowns

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -66,16 +66,18 @@ function formatDate(date) {
     return day + ' ' + monthNames[monthIndex] + ' ' + year;
   }
 
-  function dropDown(url,field) {
+  function dropDown(url,field,attr,id) {
     axios.get(url,{
         withCredentials:true
     }).then(function(result){
         var text='<option value="" disabled selected>'+field+'</option>';
         var data = result.data.classes;
         for(var i = 0;i<data.length;i++){
-            text+="<option>"+data[i].book_no+"</option>";
+            if(data[i][attr]!=undefined){
+                text+="<option>"+data[i][attr]+"</option>";
+            }
         }
-        var resultElement = document.getElementById('bookno')
+        var resultElement = document.getElementById(id)
         resultElement.innerHTML = text
     }).catch(function(err){
         console.log(err)
